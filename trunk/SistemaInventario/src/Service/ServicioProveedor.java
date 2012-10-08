4823a708ad064c8614f25f994f056b377815a8e4
gvillarg@pucp.pe <gvillarg@pucp.pe@3dd7aa47-87a0-697b-778f-c32ac5a2e519>
2012-10-08 00:15:08 +0000

    FRMCliente

---
+++ trunk/SistemaInventario/src/Service/ServicioProveedor.java
@@ -0,0 +1,78 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+import Beans.Proveedor;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Guti
+ */
+public class ServicioProveedor {
+    private int nextId=1;
+    private ArrayList<Proveedor> proveedores=new ArrayList<Proveedor>();
+    
+    public void agregarProveedor(Proveedor proveedor){
+        proveedor.setId(nextId++);
+        getProveedores().add(proveedor);
+    }
+    public Proveedor buscarProveedorId (int id){
+		for (int i=0; i<getProveedores().size(); i++)
+		{
+			if (getProveedores().get(i).getId() == id) 
+				return getProveedores().get(i);
+		}
+	
+		return null;		
+	}
+    public Proveedor buscarProveedorPos(int i){
+		Proveedor proveedor=( i<getProveedores().size() && i>=0) ? getProveedores().get(i) : null;
+		return proveedor;
+	}
+    public void eliminaProveedor (int id)
+	{
+		for (int i=0; i<getProveedores().size(); i++)
+		{
+			if(getProveedores().get(i).getId() == id)
+			{
+				getProveedores().remove(i);
+				break;
+			}
+		}
+	}
+    public void eliminaProveedor (Proveedor proveedor)
+	{
+            int id=proveedor.getId();
+            eliminaProveedor(id);
+		/*for (int i=0; i<proveedores.size(); i++)
+		{
+			if(product.getId() == provicu.get(i).getId())
+			{
+				products.remove(i);
+				break;
+			}
+		}*/
+	}
+    public void editarProveedor(Proveedor buscado)
+	{
+		Proveedor proveedor=  buscarProveedorId( buscado.getId()        );
+		if(proveedor!=null) 
+		{
+			proveedor.setId(            buscado.getId());
+			proveedor.setRuc(           buscado.getRuc());
+			proveedor.setRazonSocial(   buscado.getRazonSocial());
+			proveedor.setWebPage(       buscado.getWebPage());
+		}
+		
+	}
+
+    /**
+     * @return the proveedores
+     */
+    public ArrayList<Proveedor> getProveedores() {
+        return proveedores;
+    } 
+}
