4ce62a1574694ea9c8681069c9c6edcc5f8014f4
gvillarg@pucp.pe <gvillarg@pucp.pe@3dd7aa47-87a0-697b-778f-c32ac5a2e519>
2012-11-02 20:46:08 +0000

    Implementacion de clase abstracta HiloActualizaTabla

--- trunk/SistemaInventario/src/Hilos/HiloArticulo.java
+++ trunk/SistemaInventario/src/Hilos/HiloArticulo.java
@@ -22,31 +22,4 @@ public class HiloArticulo extends HiloActualizaTabla {
         else
             frmFiltrarArticulo.actualizarTabla();
     }
-//    FrmFiltrarArticulo frmArticulo;
-//    private boolean run=false;
-//    public HiloActualizaTablaArticulo(FrmFiltrarArticulo frmArticulo){
-//        this.frmArticulo=frmArticulo;
-//    }
-//    @Override
-//    public void run(){
-//        run=true;
-//        System.out.println("HiloTablaCliente iniciado");
-//        while(run){
-//            try {
-//                sleep(6000);
-//                frmArticulo.actualizarTabla();
-//                
-//            }catch (InterruptedException e) {
-//                System.out.println(e);
-//                close();
-//            }
-//        }
-//        System.out.println("HiloTablaCliente terminado");
-//    }
-//    public void close(){
-//        this.run=false;
-//    }
-//    public boolean isClosed(){
-//        return !run;
-//    }
 }
--- trunk/SistemaInventario/src/Hilos/HiloCliente.java
+++ trunk/SistemaInventario/src/Hilos/HiloCliente.java
@@ -12,33 +12,9 @@ import Vistas.FRMCliente;
  */
 public class HiloCliente extends HiloActualizaTabla {
     FRMCliente frmCliente;
-//    private boolean run=false;
     public HiloCliente(FRMCliente frmCliente){
         this.frmCliente=frmCliente;
     }
-//    @Override
-//    public void run(){
-//        run=true;
-//        System.out.println("HiloTablaCliente iniciado");
-//        while(run){
-//            try {
-//                sleep(6000);
-//                frmCliente.actualizarTabla();
-//                
-//            }catch (InterruptedException e) {
-//                System.out.println(e);
-//                close();//run=false;
-//            }
-//        }
-//        System.out.println("HiloTablaCliente terminado");
-//    }
-//    public void close(){
-//        this.run=false;
-//    }
-//    public boolean isClosed(){
-//        return !run;
-//    }
-
     @Override
     public void actualizarTabla() {
         if (frmCliente.isClosed())
