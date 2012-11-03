7fec0bb750f6594bde3e80d28a4c090c911b0195
gvillarg@pucp.pe <gvillarg@pucp.pe@3dd7aa47-87a0-697b-778f-c32ac5a2e519>
2012-11-03 18:20:37 +0000

    ServicioGuiaRemision

--- trunk/SistemaInventario/src/Service/ServicioGuiaRemision.java
+++ trunk/SistemaInventario/src/Service/ServicioGuiaRemision.java
@@ -5,6 +5,11 @@
 package Service;
 
 import Beans.GuiaRemision;
+import Beans.DetalleGuiaRemision;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 /**
@@ -12,12 +17,66 @@ import java.util.ArrayList;
  * @author Guti
  */
 public class ServicioGuiaRemision {
-    private ArrayList<GuiaRemision> guiasRemision = new ArrayList<GuiaRemision>();
-    private String connectionUrl ="jdbc:mysql://quilla.lab.inf.pucp.edu.pe:3306/inf282g1?" +
-            "user=inf282g1&password=anillo";
-    public void agregarGuiaRemision(GuiaRemision guiaRemision){
-		getGuiasRemision().add(guiaRemision);
-	}
+    private ArrayList<GuiaRemision> guiasRemision = null;
+    private String connectionUrl = "jdbc:mysql://quilla.lab.inf.pucp.edu.pe:3306/inf282g1?" +
+                                    "user=inf282g1&password=anillo";
+    public int agregarGuiaRemision(GuiaRemision guiaRemision){
+        int result =0;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        
+        
+        try {
+            Driver myDriver = new com.mysql.jdbc.Driver();
+            conn = DriverManager.getConnection(connectionUrl);
+            String SQLString =
+                    "INSERT INTO guia_remision "
+                    + "(motivoTranslado,fecha,origen,destino,transportista,total,idCliente) "
+                    + "VALUES(?,?,?,?,?,?,?);";
+            
+            pstmt = conn.prepareStatement(SQLString);
+            pstmt.setString(1, guiaRemision.getMotivoTranslado());
+            //pstmt.setDate(2, );
+            pstmt.setString(3,guiaRemision.getOrigen());
+            pstmt.setString(4, guiaRemision.getDestino());
+            pstmt.setFloat(5, guiaRemision.getTotal());
+            pstmt.setInt(6, guiaRemision.getCliente().getId());
+                    
+            result =  pstmt.executeUpdate(); 
+            if (result == 0)
+                throw new Exception();
+     
+            System.out.println("conexion hecha: agregarGuiaRemision");
+            DetalleGuiaRemision detalle=null;
+            for(int i=0;i<guiaRemision.getDetalle().size();i++){
+                detalle=guiaRemision.getDetalle().get(i);
+                SQLString="INSERT INTO detalle_guia_remision "
+                        + "(cantidad,idArticulo,idGuiaRemision) "
+                        + "VALUES(?,?,?);";
+                
+                pstmt=conn.prepareStatement(SQLString);
+                pstmt.setInt(1, detalle.getCantidad());
+                pstmt.setInt(2,detalle.getArticulo().getId());
+                pstmt.setInt(3, guiaRemision.getId());
+                
+                result = pstmt.executeUpdate();
+                if (result==0)
+                    throw new Exception();                
+            }
+                       
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } 
+        finally {
+             //5. Se cierra la conexión 
+             try{if(pstmt!=null) pstmt.close();}
+             catch(Exception e){e.printStackTrace();}
+             try{if(conn!=null) conn.close();}
+             catch(Exception e){e.printStackTrace();}
+        }
+        return result;
+    }
+        
     public GuiaRemision buscarGuiaRemisionId (int id){
 		for (int i=0; i<getGuiasRemision().size(); i++)
 		{
