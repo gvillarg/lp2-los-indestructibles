f8b5a30f82fb0d3720be068783f531d71b0dc5c5
gvillarg@pucp.pe <gvillarg@pucp.pe@3dd7aa47-87a0-697b-778f-c32ac5a2e519>
2012-11-15 21:44:07 +0000

    ServicioGuiaRemision final

--- SistemaInventario/src/Service/ServicioGuiaRemision.java
+++ SistemaInventario/src/Service/ServicioGuiaRemision.java
@@ -78,12 +78,74 @@ public class ServicioGuiaRemision {
         return result;
     }
     public GuiaRemision buscarGuiaRemisionId (int id){
-        getGuiasRemision();
-        for (int i=0; i<getGuiasRemision().size(); i++){
-            if (getGuiasRemision().get(i).getId() == id) 
-                return getGuiasRemision().get(i);
+        Connection conn=null;
+        PreparedStatement ps=null;
+        ResultSet rs=null,rs_detalle=null;
+        GuiaRemision guia=new GuiaRemision();
+        DetalleGuiaRemision detalle=null;
+        try{
+            Driver driver=new com.mysql.jdbc.Driver();
+            conn = DriverManager.getConnection(connectionUrl);
+            ps=conn.prepareStatement(
+                      "SELECT * FROM guia_remision"
+                    + "WHERE idGuia_Remision=?;");
+            ps.setInt(1, id);
+            rs=ps.executeQuery();
+            System.out.println("conexion hecha: buscarGuiasRemision.guia_remision");
+                
+            while(rs.next()){
+                String motivoTraslado=rs.getString("motivoTraslado");
+                Date fecha=rs.getDate("fecha");
+                String origen=rs.getString("origen");
+                String destino=rs.getString("destino");
+                String transportista=rs.getString("transportista");
+                float total=rs.getFloat("total");
+                int idCliente=rs.getInt("idCliente");
+                Cliente cliente=Main.servicioCliente.buscarClienteId(idCliente);
+                
+                guia.setId(id);
+                guia.setMotivoTranslado(motivoTraslado);
+                guia.setFecha(fecha);
+                guia.setOrigen(origen);
+                guia.setDestino(destino);
+                guia.setTransportista(transportista);
+                guia.setTotal(total);
+                guia.setCliente(cliente);
+                
+                ps=conn.prepareStatement(
+                        "SELECT * FROM detalle_guia_remision"
+                        + "WHERE idGuia_Remision=?");
+                ps.setInt(1, id);
+                rs_detalle=ps.executeQuery();
+                
+                while(rs_detalle.next()){
+                    int idDetalleGuiaRemision=rs_detalle.getInt("idDetalleGuiaRemision");
+                    int cantidad=rs_detalle.getInt("cantidad");
+                    int idArticulo=rs_detalle.getInt("idArticulo");
+                    Articulo articulo=Main.servicioArticulo.buscarArticuloId(idArticulo);
+                    
+                    detalle=new DetalleGuiaRemision();
+                    detalle.setId(idDetalleGuiaRemision);
+                    detalle.setCantidad(cantidad);
+                    detalle.setArticulo(articulo);
+                    guia.agregarDetalle(detalle);
+                }
+            }
         }
-        return null;
+        catch (Exception ex) {
+            ex.printStackTrace();
+        } 
+        finally {
+             try {if (rs_detalle != null) rs_detalle.close(); } 
+             catch(Exception e){e.printStackTrace();} 
+             try {if (rs != null) rs.close(); } 
+             catch(Exception e){e.printStackTrace();}  
+             try{if(ps!=null) ps.close();}
+             catch(Exception e){e.printStackTrace();}
+             try{if(conn!=null) conn.close();}
+             catch(Exception e){e.printStackTrace();}
+        }
+        return guia;
     }
     public GuiaRemision buscarGuiaRemisionPos(int i){
         getGuiasRemision();
@@ -121,14 +183,6 @@ public class ServicioGuiaRemision {
     public void eliminaGuiaRemision (GuiaRemision guiaRemision)	{
             int id=guiaRemision.getId();
             eliminaGuiaRemision(id);
-		/*for (int i=0; i<guiasRemision.size(); i++)
-		{
-			if(product.getId() == provicu.get(i).getId())
-			{
-				products.remove(i);
-				break;
-			}
-		}*/
 	}
     public int editarGuiaRemision(GuiaRemision guiaRemision){
         int result =0;
@@ -192,25 +246,25 @@ public class ServicioGuiaRemision {
         guiasRemision=new ArrayList<GuiaRemision>();
         Connection conn = null;
         PreparedStatement pstmt = null;
-        ResultSet rs = null;
+        ResultSet rs_guia = null, rs_detalle = null;
         try {
             Driver myDriver = new com.mysql.jdbc.Driver();
             conn = DriverManager.getConnection(connectionUrl);
             pstmt = conn.prepareStatement("SELECT * FROM guia_remision,empresa "
                     +"WHERE guia_remision.idCliente=empresa.idEmpresa AND empresa.tipo=0; ");
-            rs =  pstmt.executeQuery();
+            rs_guia =  pstmt.executeQuery();
             System.out.println("conexion hecha: getGuiasRemision.guia_remision");
             
-            while (rs.next()){
-                int idCliente=rs.getInt("idEmpresa");
-                int ruc=rs.getInt("ruc");
-                String razonSocial=rs.getString("razonSocial");
-                String webPage=rs.getString("webPage");
-                int pais=rs.getInt("pais");
-                String rubro=rs.getString("rubro");
-                String nombreC=rs.getString("nombreContacto");
-                int telefonoC=rs.getInt("telefonoContacto");
-                String emailC=rs.getString("emailContacto");  
+            while (rs_guia.next()){
+                int idCliente=rs_guia.getInt("idEmpresa");
+                int ruc=rs_guia.getInt("ruc");
+                String razonSocial=rs_guia.getString("razonSocial");
+                String webPage=rs_guia.getString("webPage");
+                int pais=rs_guia.getInt("pais");
+                String rubro=rs_guia.getString("rubro");
+                String nombreC=rs_guia.getString("nombreContacto");
+                int telefonoC=rs_guia.getInt("telefonoContacto");
+                String emailC=rs_guia.getString("emailContacto");  
                 
                 cliente= new Cliente();
                 cliente.setId(idCliente);
@@ -223,13 +277,13 @@ public class ServicioGuiaRemision {
                 cliente.setTelefonoContacto(telefonoC);
                 cliente.setEmailContacto(emailC);
                 
-                int id = rs.getInt("idGuia_Remision");
-                String motivo = rs.getString("motivoTraslado");
-                Date fecha = rs.getDate("fecha");
-                String origen=rs.getString("origen");
-                String destino=rs.getString("destino");
-                String transportista=rs.getString("transportista");
-                float total=rs.getFloat("total");    
+                int id = rs_guia.getInt("idGuia_Remision");
+                String motivo = rs_guia.getString("motivoTraslado");
+                Date fecha = rs_guia.getDate("fecha");
+                String origen=rs_guia.getString("origen");
+                String destino=rs_guia.getString("destino");
+                String transportista=rs_guia.getString("transportista");
+                float total=rs_guia.getFloat("total");    
                 
                 guiaRemision=new GuiaRemision();
                 guiaRemision.setId(id);
@@ -248,14 +302,14 @@ public class ServicioGuiaRemision {
                                 + "WHERE idGuia_Remision=?;";
                 pstmt=conn.prepareStatement(SQLString);
                 pstmt.setInt(1,guiaRemision.getId());
-                ResultSet res= pstmt.executeQuery();
+                rs_detalle= pstmt.executeQuery();
                 System.out.println("conexion hecha: getGuiasRemision.detalle_guia_remision");
                 for(int i=0;i<guiaRemision.getDetalle().size();i++){
                     detalle=new DetalleGuiaRemision();
-                    detalle.setId(res.getInt("idDetalleGuiaRemision"));
-                    articulo=Main.servicioArticulo.buscarArticuloId(res.getInt("idArticulo"));
+                    detalle.setId(rs_detalle.getInt("idDetalleGuiaRemision"));
+                    articulo=Main.servicioArticulo.buscarArticuloId(rs_detalle.getInt("idArticulo"));
                     detalle.setArticulo(articulo);
-                    detalle.setCantidad(res.getInt("cantidad"));
+                    detalle.setCantidad(rs_detalle.getInt("cantidad"));
                     guiaRemision.agregarDetalle(detalle);
                 }
                 guiasRemision.add(guiaRemision);
@@ -265,8 +319,9 @@ public class ServicioGuiaRemision {
             ex.printStackTrace();
         } 
         finally {
-             //5. Se cierra la conexión
-             try {if (rs != null) rs.close(); } 
+             try {if (rs_detalle != null) rs_detalle.close(); } 
+             catch(Exception e){e.printStackTrace();} 
+             try {if (rs_guia != null) rs_guia.close(); } 
              catch(Exception e){e.printStackTrace();}  
              try{if(pstmt!=null) pstmt.close();}
              catch(Exception e){e.printStackTrace();}
