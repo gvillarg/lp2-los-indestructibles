6f9072422194f70cba75b08e0ed75c5121147971
gvillarg@pucp.pe <gvillarg@pucp.pe@3dd7aa47-87a0-697b-778f-c32ac5a2e519>
2012-11-15 23:42:29 +0000

    ServicioUsuario v2

--- SistemaInventario/src/Service/ServicioUsuario.java
+++ SistemaInventario/src/Service/ServicioUsuario.java
@@ -1,16 +1,49 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Service;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  *
  * @author Guti
  */
 public class ServicioUsuario {
+    static private String connectionUrl ="jdbc:mysql://quilla.lab.inf.pucp.edu.pe:3306/inf282g1?" +
+        "user=inf282g1&password=anillo";
     static public boolean AutentificarUsuario(String user,String pass){
-        
-        return true;
+        boolean resultado=false;
+        Connection conn=null;
+        PreparedStatement ps=null;
+        ResultSet rs=null;
+        try{
+            Driver driver=new com.mysql.jdbc.Driver();
+            conn = DriverManager.getConnection(connectionUrl);            
+            
+            ps = conn.prepareStatement("SELECT * FROM usuarios "
+                                +"WHERE usuario=? and contrasena=?;");
+            ps.setString(1, user);
+            ps.setString(2, pass);
+            rs = ps.executeQuery();
+            System.out.println("ServicioUsuario.AutentificarUsuario: conexion hecha");
+            
+            if(rs.next()){
+                resultado=true;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }finally {
+             try{if(ps!=null) ps.close();}
+             catch(Exception e){e.printStackTrace();}
+             try{
+                 if(conn!=null){
+                   conn.close();
+                   System.out.println("ServicioUsuario.AutentificarUsuario: conexion cerrada");
+                 }
+             }catch(Exception e){e.printStackTrace();}
+        }                
+        return resultado;
     }
 }
