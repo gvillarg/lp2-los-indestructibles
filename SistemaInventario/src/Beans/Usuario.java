c14dd710a19ea381d56c428506d5ad105fde682d
gvillarg@pucp.pe <gvillarg@pucp.pe@3dd7aa47-87a0-697b-778f-c32ac5a2e519>
2012-11-18 14:18:10 +0000

    GuiaRemison

---
+++ SistemaInventario/src/Beans/Usuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Guti
+ */
+public class Usuario implements Serializable{
+    private int id;
+    private String usuario;
+    
+}
