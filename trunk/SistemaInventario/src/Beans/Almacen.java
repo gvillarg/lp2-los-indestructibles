faa541c0320ab74fae414f0c33bb83e8f223bafe
andrest16@gmail.com <andrest16@gmail.com@3dd7aa47-87a0-697b-778f-c32ac5a2e519>
2012-11-22 12:12:27 +0000

    habilitando el login del usuario

--- trunk/SistemaInventario/src/Beans/Almacen.java
+++ trunk/SistemaInventario/src/Beans/Almacen.java
@@ -4,13 +4,14 @@
  */
 package Beans;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  *
  * @author Guti
  */
-public class Almacen {
+public class Almacen implements Serializable {
     private int id;
     private String direccion;
     private String Area;
