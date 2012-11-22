e0f0a90a951b08ad38c5e102853449f59dfefef3
andrest16@gmail.com <andrest16@gmail.com@3dd7aa47-87a0-697b-778f-c32ac5a2e519>
2012-11-22 05:10:27 +0000

    habilitando el login del usuario

--- trunk/SistemaInventario/src/Beans/Usuario.java
+++ trunk/SistemaInventario/src/Beans/Usuario.java
@@ -14,6 +14,7 @@ import java.io.Serializable;
  *
  * @author andrés
  */
+@SuppressWarnings("serial")
 public class Usuario implements Serializable {
     private String login;
     private transient String password;
